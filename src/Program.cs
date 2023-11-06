90039ddb3b92ea180547e022edcda14a85ca9a7a
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2023-11-06 13:36:57 +0800

    bug fix

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,14 @@ using System.Text.Json;
 using IFers.RCloneDriver;
 using Microsoft.Extensions.Logging;
 
+return args switch
+{
+    ["init", _] => await Init(args[1]),
+    ["clone", ..] => await Clone(args[1..]),
+    [] => await Sync(),
+    _ => -1
+};
+
 async Task RClone(string[] args)
 {
     var proc = new Process();
@@ -31,6 +39,22 @@ async Task<RcdConfig?> ReadConfig(string path)
     }
 }
 
+(RcdConfig, int) MergeConfig(RcdConfig config1, RcdConfig config2)
+{
+    var (src, des) = (config1, config2);
+    if (src.Timestamp == des.Timestamp) return (config1, 0);
+    if (src.Timestamp < des.Timestamp) (src, des) = (des, src);
+    return (src with
+    {
+        Timestamp = DateTime.Now
+    }, src == config1 ? 1 : -1); // if config1 is latest, return 1, else -1
+}
+
+void Log(LogLevel level, string format, params object[] args)
+{
+    Console.WriteLine($"[{level}][{DateTime.Now:HH:mm:ss}]{format}", args);
+}
+
 async Task<bool> WriteConfig(string path, RcdConfig config)
 {
     try
@@ -42,22 +66,6 @@ async Task<bool> WriteConfig(string path, RcdConfig config)
     {
         return false;
     }
-}
-
-void Log(LogLevel level, string format, params object[] args)
-{
-    Console.WriteLine($"[{level}][{DateTime.Now:HH:mm:ss}]{format}", args);
-}
-
-(RcdConfig, int) MergeConfig(RcdConfig config1, RcdConfig config2)
-{
-    var (src, des) = (config1, config2);
-    if (src.Timestamp == des.Timestamp) return (config1, 0);
-    if (src.Timestamp < des.Timestamp) (src, des) = (des, src);
-    return (src with
-    {
-        Timestamp = DateTime.Now
-    }, src == config1 ? 1 : -1); // if config1 is latest, return 1, else -1
 }
 
 async Task<int> Clone(string[] xargs)
@@ -75,29 +83,6 @@ async Task<int> Clone(string[] xargs)
         "copy",
         remote,
         local
-    });
-    return 0;
-}
-
-async Task<int> Init(string remote)
-{
-    if (Directory.Exists(".rcd"))
-    {
-        Log(LogLevel.Error, "Repo already exists.");
-        return 1;
-    }
-
-    Directory.CreateDirectory(".rcd");
-    var conf = new RcdConfig(remote, DateTime.Now, new List<FilterOption>
-    {
-        new FilterOption(FilterOptionType.FilesMatchingPattern, "node_modules/")
-    });
-    await WriteConfig(".rcd/conf.json", conf);
-    await RClone(new[]
-    {
-        "copy",
-        "./.rcd",
-        $"{conf.Remote}/.rcd"
     });
     return 0;
 }
@@ -181,27 +166,43 @@ async Task<int> Sync()
             "\"+ .rcd/\""
         })
         .ToArray();
-    await RClone(args.Append("--dry-run").ToArray());
+    await RClone(@params.Append("--dry-run").ToArray());
     await Console.Out.WriteLineAsync(
         "Dry run completed, type C/i/q to [Continue(default)],[Use Interactive], or [Quit(other characters)].");
     var cmd = Console.ReadLine();
-    if (cmd is "c" or "")
+    switch (cmd)
     {
-        await RClone(args);
-    }
-    else if (cmd is "i")
-    {
-        await RClone(args.Append("--interactive").ToArray());
-        ;
+        case "c" or "":
+            await RClone(@params);
+            break;
+        case "i":
+            await RClone(@params.Append("--interactive").ToArray());
+            ;
+            break;
     }
 
     return 0;
 }
 
-return args switch
+async Task<int> Init(string remote)
 {
-    ["init", _] => await Init(args[1]),
-    ["clone", ..] => await Clone(args[1..]),
-    [] => await Sync(),
-    _ => -1
-};
+    if (Directory.Exists(".rcd"))
+    {
+        Log(LogLevel.Error, "Repo already exists.");
+        return 1;
+    }
+
+    Directory.CreateDirectory(".rcd");
+    var conf = new RcdConfig(remote, DateTime.Now, new List<FilterOption>
+    {
+        new FilterOption(FilterOptionType.FilesMatchingPattern, "- node_modules/")
+    });
+    await WriteConfig(".rcd/conf.json", conf);
+    await RClone(new[]
+    {
+        "copy",
+        "./.rcd",
+        $"{conf.Remote}/.rcd"
+    });
+    return 0;
+}
